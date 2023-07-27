8a4caa088828ad336b79417112e2657cff1e2f0c
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-07-27 22:31:35 +0200

    update

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,84 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  // getFullList = async () => {
+  //   const response = await fetch("http://localhost:8000/characters");
+  //   console.log(response);
+  //   if (response.status === 200) {
+  //     return response.json();
+  //   }
+  // };
+  getFulCharlList = async () => {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters`);
+      console.log("my response:", response);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  }
+  // getOneChar = async (id) => {
+  //   const response = await fetch("http://localhost:8000/characters/:id");
+  //   if (response.status === 200) {
+  //     return response.json();
+  //   }
+  // };
+  getOneChar = async (id) => {
+    try {
+      const url = `${this.BASE_URL}/characters/${id}`;
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  getOneRegister () {
+  createOneRegister = async (oneCharData) => {
+    try {
+      const response = await axios.post(
+        `${this.BASE_URL}/characters`,
+        oneCharData
+      );
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log("error occurred while creating: ", error);
+      return oneCharData;
+    }
+  };
 
-  }
+  updateOneRegister = async (id, CharUpDate) => {
+    try {
+      const response = await axios.patch(
+        `${this.BASE_URL}/characters/${id}`,
+        CharUpDate
+      );
+      console.log("response :", response);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log("error occurred while updating: ", error);
+    }
+  };
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister = async (id) => {
+    try {
+      const response = await axios.delete(`${this.BASE_URL}/characters/${id}`);
+      console.log("response :", response);
+      if (response.status === 200) {
+        return true; // when deleted
+      }
+    } catch (error) {
+      console.log("error ocurred while deleting : ", error);
+    }
+  };
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,150 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", async function (event) {
+      try {
+        //get all characters from API
+        const getAllChar = await charactersAPI.getFulCharlList();
+        displayCharacters(getAllChar);
+      } catch (error) {}
+    });
 
-  });
+  // function to display
+  function displayCharacters(characters) {
+    // variable container to store our characters
+    const charsContainer = document.querySelector(".characters-container");
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+    charsContainer.innerHTML = "";
+    // loop through
+    characters.forEach((character) => {
+      const characterCard = document.createElement("div");
+      characterCard.className = "character-info";
+      charsContainer.appendChild(characterCard);
+      characterCard.innerHTML = `
+      <div class="id">Id: ${character.id}</div>
+      <div class="name">Name: ${character.name}</div>
+      <div class="occupation">Occupation: ${character.occupation}</div>
+      <div class="cartoon">Is a Cartoon?: ${character.cartoon}</div>
+      <div class="weapon">Weapon: ${character.weapon}</div>`;
+    });
+  }
 
-  });
+  // fetch one
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", async function (event) {
+      // variable to store the dispalyed character
+      const toDisplayChr = document.querySelector("#fetchOne-input").value;
+      try {
+        const fetchOne = await charactersAPI.getOneChar(toDisplayChr);
+        //call function
+        displayOneChar(fetchOne);
+      } catch (error) {}
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  //function to fetch one
+  function displayOneChar(fetchOne) {
+    // resrendering it on same div of characters container instead of adding another div
+    const charsContainer = document.querySelector(".characters-container");
+    // empty container
+    charsContainer.innerHTML = "";
+    // if statement to check character by id
 
-  });
+    if (fetchOne) {
+      //Create the new div
+      const characterCard = document.createElement("div");
+      characterCard.className = "character-info";
+      charsContainer.appendChild(characterCard);
+      characterCard.innerHTML = `
+      <div class="id">Id: ${fetchOne.id}</div>
+      <div class="name">Name: ${fetchOne.name}</div>
+      <div class="occupation">Occupation: ${fetchOne.occupation}</div>
+      <div class="cartoon">Is a Cartoon?: ${fetchOne.cartoon}</div>
+      <div class="weapon">Weapon: ${fetchOne.weapon}</div>`;
+    }
+  }
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  // delete
+  // document
+  //   .getElementById("delete-one")
+  //   .addEventListener("click", function (event) {
 
-  });
+  //   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", async function (event) {
+      // input value to get the ID
+      const charId = document.querySelector("#delete-input").value;
 
-  });
+      //If an ID is entered in input
+      if (charId) {
+        const deleteChar = await charactersAPI.deleteOneRegister(charId);
+
+        if (deleteChar) {
+          document.getElementById("delete-one").style.backgroundColor = "green";
+        } else {
+          document.getElementById("delete-one").style.backgroundColor = "red";
+        }
+      }
+    });
+
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", async function (event) {
+      //input values with corresponing ids
+      const idEdit = document.querySelector("#id-edit").value;
+      const NameEdit = document.querySelector("#name-edit").value;
+      const occupationEdit = document.querySelector("#occupation-edit").value;
+      const weaponEdit = document.querySelector("#weapon-edit").value;
+      const cartoonEdit = document.querySelector("#cartoon-edit").checked;
+
+      //store gotten value in object just like fake api
+      const editCharacter = {
+        name: NameEdit,
+        occupation: occupationEdit,
+        weapon: weaponEdit,
+        cartoon: cartoonEdit,
+      };
+
+      //variable to edit chararacters  on API
+      const toEditChar = await charactersAPI.updateOneRegister(
+        idEdit,
+        editCharacter
+      );
+      if (toEditChar) {
+        document.getElementById("edit-data").style.backgroundColor = "green";
+      } else {
+        document.getElementById("edit-data").style.backgroundColor = "red";
+      }
+    });
+
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", async function (event) {
+      //input values with corresponing ids
+      const name = document.querySelector("#new-name").value;
+      const occupation = document.querySelector("#new-occupation").value;
+      const weapon = document.querySelector("#new-weapon").value;
+      const cartoon = document.querySelector("#new-cartoon").checked;
+
+      // destructuring, use variable name as field name
+      const newCharacter = {
+        name,
+        occupation,
+        weapon,
+        cartoon,
+      };
+
+      //to store our
+      const toAddChar = await charactersAPI.createOneRegister(newCharacter);
+
+      if (toAddChar) {
+        document.getElementById("send-data").style.backgroundColor = "green";
+      } else {
+        document.getElementById("send-data").style.backgroundColor = "red";
+      }
+    });
 });
